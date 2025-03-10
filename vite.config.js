bae468e6a436c814515f6b388b287e9206f60be1
oubayhamzaoui5 <hamzaouioubay@isamm3u-manouba.tn>
2025-03-10 09:29:41 +0100

    Save current work before updating video paths

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/spxcepower/', // Add this line to specify the subdirectory
   plugins: [react()],
 })
